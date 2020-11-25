ee5e6032d301b076aaf2ab95c49c2ce614449e71
Yoshida-tk <taiki610@outlook.jp>
2020-11-25 18:26:17 +0900

    テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :item do
-    category_id     {'2'}
-    status_id       {'3'}
-    area_id         {'4'}
-    shipping_id     {'5'}
-    days_id         {'6'}
+    category_id     {2}
+    status_id       {3}
+    area_id         {4}
+    shipping_id     {5}
+    days_id         {6}
     product_name    {'test'}
-    price           {'3000'}
+    price           {3000}
     text            {'test'}
     association :user
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,12 +29,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Product name can't be blank")
       end
       it 'カテゴリーの情報がなければ登録できない' do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
       it '商品の状態についての情報がなければ登録できない' do
-        @item.status_id ="1"
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
@@ -44,17 +44,17 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Text can't be blank")
       end
       it '配送料の負担についての情報がなければ登録できない' do
-        @item.shipping_id = "1"
+        @item.shipping_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping must be other than 1")
       end
       it '発送元の地域についての情報がなければ登録できない' do
-        @item.area_id = "1"
+        @item.area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Area must be other than 1")
       end
       it '発送までの日数についての情報がなければ登録できない' do
-        @item.days_id = "1"
+        @item.days_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Days must be other than 1")
       end
@@ -64,12 +64,12 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格の範囲が¥300以上でなければ登録できない' do
-        @item.price ="299"
+        @item.price =299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '価格の範囲が¥9,999,999以下でなければ登録できない' do
-        @item.price = "10,000,000"
+        @item.price = 10,000,000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is invalid")
       end
