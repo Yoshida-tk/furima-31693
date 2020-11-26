61a28581074d37f55ba065839feb9de8e44f70f2
Yoshida-tk <taiki610@outlook.jp>
2020-11-26 17:42:15 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,9 +21,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   
   def item_params
     params.require(:item).permit(:category_id, :status_id, :area_id, :days_id, :shipping_id, :product_name, :price, :text, :image).merge(user_id: current_user.id)
   end
+
 end
