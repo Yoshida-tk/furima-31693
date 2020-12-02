760b03f33081cd70cfccfa1dd8aeeb7956324046
Yoshida-tk <taiki610@outlook.jp>
2020-12-02 16:54:17 +0900

    Revert "Revert "Revert "売却済み商品の購入ページを表示させないようにする"""
    This reverts commit 61b4f679ac6fd48018a1aa715a69aa770309434f.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
-
+  before_action :set_order, only: [:index]
   def index
     @order_address = OrderAddress.new
-    if @item.order.present?
-      if current_user.id == @item.user_id || current_user.id != @item.user_id
-        redirect_to root_path
-      else
-        render 'index'
-      end
-    end
   end
 
   def create
@@ -43,4 +36,10 @@ class OrdersController < ApplicationController
     )
   end
 
+  def set_order
+    @item = Item.find(params[:item_id])
+      redirect_to root_path
+    unless current_user == @item.user
+    end
+  end
 end
